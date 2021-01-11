94f98e03e83b559a67fef711160e470a1a945aee
MatStat <powdersteamboat@gmail.com>
2021-01-11 14:59:51 -0700

    Added if/else statements for adding 0 to values

--- functions.js
+++ functions.js
@@ -3,6 +3,11 @@ let seconds= 0;
 let minutes= 0;
 let hours= 0;
 
+//vars to hold display values
+let displaySeconds = 0;
+let displayMinutes = 0;
+let displayHours = 0;
+
 //stopwatch functions
 function stopWatch(){
 
@@ -18,6 +23,26 @@ function stopWatch(){
                 hours++;
             }
     }
+
+    //adds leading 0 to value if there is only one digit
+if(seconds < 10){
+    displaySeconds = "0" + seconds.toString();
+}
+
+else{
+    displaySeconds = seconds;
+}
+
+if(minutes < 10){
+    displayMinutes = "0" + minutes.toString();
+}
+if(hours <10){
+    displayHours = "0" + hours.toString();
+}
+
+else{
+    displayHours = hours;
+}
 
     //Displaying updated time values to users
     document.getElementById("display").innerHTML = hours + ":" + minutes + ":" + seconds;
