64ddcdf698e15f271c427cf4725f06912447763b
MatStat <powdersteamboat@gmail.com>
2021-01-11 15:32:20 -0700

    Added startStop function, created reset function, updated display

--- functions.js
+++ functions.js
@@ -8,6 +8,12 @@ let displaySeconds = 0;
 let displayMinutes = 0;
 let displayHours = 0;
 
+//var for setInterval() function
+let interval = null;
+
+//var for stopwatch status
+let status = "stopped";
+
 //stopwatch functions
 function stopWatch(){
 
@@ -36,7 +42,7 @@ else{
 if(minutes < 10){
     displayMinutes = "0" + minutes.toString();
 }
-if(hours <10){
+if(hours < 10){
     displayHours = "0" + hours.toString();
 }
 
@@ -45,8 +51,36 @@ else{
 }
 
     //Displaying updated time values to users
-    document.getElementById("display").innerHTML = hours + ":" + minutes + ":" + seconds;
+    document.getElementById("display").innerHTML = displayHours + ":" + displayMinutes + ":" + displaySeconds;
 
 }
 
-window.setInterval(stopWatch, 1000);
+
+function startStop(){
+
+    if(status === "stopped"){
+
+        //start the stopwatch by calling the setInterval() function
+        interval = window.setInterval(stopWatch, 1000);
+        document.getElementById("startStop").innerHTML = "Stop";
+        status = "started";
+    }
+
+    else{
+
+        window.clearInterval(interval);
+        document.getElementById("startStop").innerHTML = "Start";
+        status = "stopped";
+    }
+}
+
+//Function to reset the stopwatch
+function reset(){
+
+    window.clearInterval(interval);
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+    document.getElementById("display").innerHTML = "00:00:00";
+    document.getElementById("startStop").innerHTML = "Start";
+}
